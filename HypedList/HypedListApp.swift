27bec0cc9b722008cf788ee6ddb624703fd07558
Bikram Aryal <bikram.aryall@gmail.com>
2022-06-13 19:57:54 +0545

    Initial Commit

---
+++ HypedList/HypedListApp.swift
@@ -0,0 +1,17 @@
+//
+//  HypedListApp.swift
+//  HypedList
+//
+//  Created by Bikram Aryal on 13/06/2022.
+//
+
+import SwiftUI
+
+@main
+struct HypedListApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
