8e4445dd2f79a9abce8dc44bede62e6d274f7dbe
Bikram Aryal <bikram.aryall@gmail.com>
2022-06-13 20:22:21 +0545

    Displaying Views in sheet

---
+++ HypedList/CreateHypedEventView.swift
@@ -0,0 +1,20 @@
+//
+//  CreateHypedEventView.swift
+//  HypedList
+//
+//  Created by Bikram Aryal on 13/06/2022.
+//
+
+import SwiftUI
+
+struct CreateHypedEventView: View {
+    var body: some View {
+        Text("Create Hyped Event")
+    }
+}
+
+struct CreateHypedEventView_Previews: PreviewProvider {
+    static var previews: some View {
+        CreateHypedEventView()
+    }
+}
--- HypedList/UpcomingView.swift
+++ HypedList/UpcomingView.swift
@@ -8,15 +8,25 @@
 import SwiftUI
 
 struct UpcomingView: View {
+    
+    @State var showingCreateView = false
+    
+    
+    
     var body: some View {
         Text("Upcoming View")
             .navigationTitle("Upcoming")
             .toolbar {
                 Button {
+                    showingCreateView = true
                     
                 } label: {
                     Image(systemName: "calendar.badge.plus")
                         .font(.title)
+                }
+                .sheet(isPresented: $showingCreateView) {
+                    CreateHypedEventView()
+                    
                 }
 
             }
