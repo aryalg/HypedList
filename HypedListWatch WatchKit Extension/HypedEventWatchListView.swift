5b6ec7a71571757717503338eed498eec7fb34e0
Bikram Aryal <bikram.aryall@gmail.com>
2022-06-18 00:51:34 +0545

    List Design and Extensions - Watch

--- HypedListWatch WatchKit Extension/HypedEventWatchListView.swift
+++ HypedListWatch WatchKit Extension/HypedEventWatchListView.swift
@@ -12,12 +12,62 @@ struct HypedEventWatchListView: View {
     var hypedEvents: [HypedEvent]
     
     var body: some View {
-        Text(/*@START_MENU_TOKEN@*/"Hello, World!"/*@END_MENU_TOKEN@*/)
+        if(hypedEvents.count > 0) {
+            
+            ScrollView {
+                VStack(spacing: 10) {
+                    ForEach(hypedEvents) {hypedEvent in
+                        HStack {
+                            Spacer()
+                           
+                        VStack {
+                            
+                        Text(hypedEvent.title)
+                                .font(.headline)
+                               
+                            
+                            
+                        Text("\(hypedEvent.timeFromNow().capitalized) on \(hypedEvent.dateAsString())")
+                            
+                        }
+                        .foregroundColor(hypedEvent.color.isDarkColor ? .white : .black)
+                        .multilineTextAlignment(.center)
+                            
+                        .padding(.horizontal, 5)
+                            Spacer()
+                        
+                          
+                        }.background(hypedEvent.color)
+                            .cornerRadius(5)
+                    }
+                }
+            }
+            
+        } else {
+        Text("No Upcoming Events\nCreate Event from Phone")
+                .multilineTextAlignment(.center)
+        }
     }
 }
 
 struct HypedEventWatchListView_Previews: PreviewProvider {
     static var previews: some View {
-        HypedEventWatchListView(hypedEvents: [testHypedEvent1, testHypedEvent2])
+        Group {
+            HypedEventWatchListView(hypedEvents: [testHypedEvent1, testHypedEvent2])
+            
+            HypedEventWatchListView(hypedEvents: [])
+        }
+    }
+}
+
+extension Color {
+    var isDarkColor: Bool {
+        var r,g,b,a: CGFloat
+        (r,g,b,a) = (0,0,0,0)
+        
+        UIColor(self).getRed(&r, green: &g, blue: &b, alpha: &a)
+        let lum = 0.2126 * r + 0.7152 * g + 0.0722 * a
+        return lum < 0.6
+        
     }
 }
